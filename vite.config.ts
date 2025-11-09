6310c30689ae663a9960450e0cde52a8d36f03c1
Mahmoud Ali <drmahmoodali91@gmail.com>
2025-11-09 05:10:42 +0200

    Fix package.json JSON parse error and add dynamic base to vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/My-Portfolio/',
+export default defineConfig(({ command, mode }) => ({
+  base: mode === 'production' ? '/My-Portfolio/' : '/',
   plugins: [react()],
-})
+}))
